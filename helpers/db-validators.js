f12311d5f87d0e01df0e207fe5c3da7cc688e060
cayetanogb22 <cayetanogb22@gmail.com>
2022-02-26 17:47:13 +0100

    CRUD Productos, categorias, y busquedas

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,29 +1,51 @@
+const { Categoria, Producto } = require('../models');
 const Role = require('../models/rol');
 const Usuario = require('../models/usuario');
 
-const esRoleValido = async (rol = '') => {
+// Validar rol
+const esRoleValido = async(rol = '') => {
     const existeRol = await Role.findOne({ rol });
     if ( !existeRol ) {
         throw new Error(`El rol ${ rol } no esta registrado en la BD`);
     }
 }
 
-const emailExiste = async (correo = '') => {
+// Validar email
+const emailExiste = async(correo = '') => {
     const existeEmail = await Usuario.findOne({ correo });
     if ( existeEmail ) {
         throw new Error(`El correo: ${ correo } ya esta registrado`);
     }
 }
 
-const existeUsuarioPorId = async (id) => {
+// Validar usuario
+const existeUsuarioPorId = async(id) => {
     const existeUsuario = await Usuario.findById(id);
     if ( !existeUsuario ) {
         throw new Error(`El usuario con id: ${ id } no existe`);
     }
 }
 
+// Validar categoria
+const existeCategoria = async(id)=> {
+    const categoria = await Categoria.findById(id);
+    if (!categoria) {
+        throw new Error(`La categoria con id: ${ id } no existe`);
+    }
+}
+
+// Validar producto
+const existeProducto = async(id)=> {
+    const producto = await Producto.findById(id);
+    if (!producto) {
+        throw new Error(`El producto con id: ${ id } no existe`);
+    }
+}
+
 module.exports = {
     esRoleValido,
     emailExiste,
-    existeUsuarioPorId
+    existeUsuarioPorId,
+    existeCategoria,
+    existeProducto
 }
